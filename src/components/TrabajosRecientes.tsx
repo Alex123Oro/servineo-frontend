6400dc1fbdc16b0557085e20877ca7935903b592
Alex123Oro <118484282+Alex123Oro@users.noreply.github.com>
2025-10-23 01:06:47 -0400

    Update TrabajosRecientes.tsx

--- src/components/TrabajosRecientes.tsx
+++ src/components/TrabajosRecientes.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState } from 'react'
+import React from 'react'
 import jobs from '../jsons/jobs.json'
 import { JobCard } from './JobCard'
 import { JobListEmpty } from './JobListEmpty'
