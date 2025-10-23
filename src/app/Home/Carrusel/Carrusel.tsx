acd132a2fd931225945a926cb4087a0c18fba349
Alex123Oro <118484282+Alex123Oro@users.noreply.github.com>
2025-10-23 00:08:38 -0400

    Update Carrusel.tsx

--- src/app/Home/Carrusel/Carrusel.tsx
+++ src/app/Home/Carrusel/Carrusel.tsx
@@ -99,14 +99,14 @@ const Carrusel = () => {
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
     };
-  }, [currentIndex]);
+  }, [nextSlide,prevSlide]);
 
   useEffect(() => {
     const interval = setInterval(() => {
       nextSlide();
     }, 8000); // Change slide every 5 seconds
     return () => clearInterval(interval);
-  }, [currentIndex]);
+  }, [nextSlide]);
 
   return (
     <div 
