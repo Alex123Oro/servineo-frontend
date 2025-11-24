4406a099b5d622df8b7e196abfcdd3c9bfa89d08
Valeria2805 <202203392@est.umss.edu>
2025-11-23 21:38:03 -0400

    Correcion de menu de login email

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -87,7 +87,7 @@ export default function LoginPage() {
         });
 
         setTimeout(() => {
-          router.push("/");
+          window.location.href = "/";
         }, 1000);
       } else {
         const mensajeError =
