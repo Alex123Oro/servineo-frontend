2838f543e3c864a263053aa760571ae854ff4222
Robert228-cod <robertlopezcaballero13@gmail.com>
2025-12-01 18:39:09 -0400

    paginas y componentes faltanates agragados

--- src/Components/Home/Footer-section.tsx
+++ src/Components/Home/Footer-section.tsx
@@ -31,7 +31,7 @@ export default function FooterSection({ onRestartTour }: FooterSectionProps = {}
   const exploreLinks = [
     { name: 'Servicios', path: '/servicios' },
     { name: 'Ofrece tus servicios', path: '/info/reparador' },
-    { name: 'Ofertas de trabajo', path: 'ofertas' },
+    { name: 'Ofertas de trabajo', path: '/job-offer-list' },
   ];
 
   return (
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
       <HowItWorksSection />
 
       <CTASection />
-      <FooterSection />
+      {/*<FooterSection />*/}
     </div>
   );
 }
